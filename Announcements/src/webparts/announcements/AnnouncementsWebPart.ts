c02d9b448187ea9fd3f6ea851616b91488214309
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-11-05 18:33:01 +0530

    Update Announcements/src/webparts/announcements/AnnouncementsWebPart.ts

--- Announcements/src/webparts/announcements/AnnouncementsWebPart.ts
+++ Announcements/src/webparts/announcements/AnnouncementsWebPart.ts
@@ -70,11 +70,11 @@ export default class AnnouncementsWebPart extends BaseClientSideWebPart<IAnnounc
             }
           });
           this.domElement.querySelector('.carousel-inner').innerHTML = html + `<div style="float:left;padding-right: 1%;">
-          <a class="glyphicon glyphicon-chevron-left btn btn-warning" style="position: absolute; top: 90%; right: 90%;height:30px;" href="#AnnouncementsDisplay" data-slide="prev"></a>
+          <a class="glyphicon glyphicon-chevron-left btn btn-warning" data-toggle="tooltip" data-placement="bottom" title="click for previous item" style="position: absolute; top: 90%; right: 90%;height:30px;" href="#AnnouncementsDisplay" data-slide="prev"></a>
           </div>
           
           <div>
-          <a class="glyphicon glyphicon-chevron-right btn btn-warning" style="position: absolute; top: 90%; right: 80%;height:30px;" href="#AnnouncementsDisplay" data-slide="next"></a>
+          <a class="glyphicon glyphicon-chevron-right btn btn-warning" data-toggle="tooltip" data-placement="bottom" title="click for next item" style="position: absolute; top: 90%; right: 80%;height:30px;" href="#AnnouncementsDisplay" data-slide="next"></a>
           </div>
           <div id="viewall">
             <a class="btn btn-warning" data-toggle="tooltip" data-placement="bottom" title="Click to see all Announcements" href="https://acuvateuk.sharepoint.com/sites/TrainingDevSite/Lists/Announcements/AllItems.aspx" target="_blank" style="position: absolute; top: 90%; left: 80%;height:30px;">View All</a>
